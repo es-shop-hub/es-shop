62e72c28a85bedd53369f3e15086cda50c1c643e
es-shop-hub <contact.es.shop1@gmail.com>
2026-04-09 12:45:45 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ import {
 } from './firebase.js';
 
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
-
+import './receipt.js';
 // --- OFFLINE ---
 enableIndexedDbPersistence(db).catch(() => {});
 
